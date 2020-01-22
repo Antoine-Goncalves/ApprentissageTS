39a359c3ebcedd979ba928e4f47dd18cb36f0f76
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-22 16:05:55 +0100

    :zap: add content for function addAndHandle

--- app.ts
+++ app.ts
@@ -6,7 +6,10 @@ function printResult(num: number) {
   console.log("Result is " + num);
 }
 
-function addAndHandle(n1: number, n2: number, cb: () => void) {}
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
 
 let combineValues: (a: number, b: number) => number;
 
