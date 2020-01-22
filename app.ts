1e99a01f541fd85c7d8318e9be6e23cf2f2ca7ae
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-22 13:04:41 +0100

    :fire: add if method for showResult

--- app.ts
+++ app.ts
@@ -2,8 +2,11 @@ function add(n1: number, n2: number, showResult: boolean) {
   // if (typeof n1 !== "number" || typeof n2 !== "number") {
   //   throw new Error("Incorrect Input!");
   // }
-
-  return n1 + n2;
+  if (showResult) {
+    console.log(n1 + n2);
+  } else {
+    return n1 + n2;
+  }
 }
 
 const number1 = 14;
