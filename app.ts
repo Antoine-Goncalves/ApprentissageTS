1f17eab44ac231ed86dbc012edd164ea6dd317a6
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-22 16:31:29 +0100

    :fire: add function generateError

--- app.ts
+++ app.ts
@@ -7,3 +7,12 @@ userInput = 14;
 if (typeof userInput === "string") {
   userName = userInput;
 }
+
+function generateError(message: string, code: number) {
+  throw {
+    message: message,
+    errorCode: code
+  };
+}
+
+generateError("What's an error", 500);
