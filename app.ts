8fb5ed02c60cfbc1dfe255b49ac8b537b1c248f1
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-22 14:40:35 +0100

    :fire: add new params for literal types

--- app.ts
+++ app.ts
@@ -1,15 +1,27 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: string
+) {
   let result;
   if (typeof input1 === "number" && typeof input2 === "number") {
     result = input1 + input2;
   } else {
     result = input1.toString() + input2.toString();
   }
-  return result;
+
+  if (resultConversion === "as-number") {
+    return +result;
+  } else {
+    return result.toString();
+  }
 }
 
-const combinedAges = combine(23, 23);
+const combinedAges = combine(23, 23, "as-number");
 console.log(combinedAges);
 
-const combinedNames = combine("Antoine", "Kelly");
+const combinedNamesAges = combine("Antoine", 23, "as-number");
+console.log(combinedNamesAges);
+
+const combinedNames = combine("Antoine", "Kelly", "as-text");
 console.log(combinedNames);
