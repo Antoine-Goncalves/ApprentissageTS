f7416266763d0138809c8b634cc3c9dac1372da4
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-22 14:33:52 +0100

    :zap: test for union types

--- app.ts
+++ app.ts
@@ -1,5 +1,15 @@
-function combine(input1: number, input2: number) {
-  const result = input1 + input2;
-
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
   return result;
 }
+
+const combinedAges = combine(23, 23);
+console.log(combinedAges);
+
+const combinedNames = combine("Antoine", "Kelly");
+console.log(combinedNames);
